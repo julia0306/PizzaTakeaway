3f5f5211fa994a452e996e57046d1415ea41cded
julia0306 <123451628+julia0306@users.noreply.github.com>
2024-02-24 16:25:38 +0100

    Ingredients properly displayed

--- ProjetPizza/Program.cs
+++ ProjetPizza/Program.cs
@@ -3,11 +3,11 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 var listePizzas = new List<Pizza>()
 {
-    new Pizza("4 fromages", 11.5f, true),
-    new Pizza("margherita", 8f, true),
-    new Pizza("indienne", 12.5f, false),
-    new Pizza("calzone", 11.5f, true),
-    new Pizza("mexicaine", 13.5f, false)
+    new Pizza("4 fromages", 11.5f, true, new List<string> { "tomate", "champignons", "oignons", "mozzarella", "cantal", "gorgonzola", "edam" }),
+    new Pizza("margherita", 8f, true, new List < string > { "mozzarella", "tomate", "champignons", "oignons", "poivrons" }),
+    new Pizza("indienne", 12.5f, false, new List < string > { "mozzarella", "sauce tomate","curry", "poulet", "champignons", "oignons" }),
+    new Pizza("calzone", 11.5f, true, new List < string > { "mozzarella", "tomate", "champignons", "oignons", "cheddar" }),
+    new Pizza("mexicaine", 13.5f, false, new List < string > { "mozzarella", "tomate", "merguez", "poulet", "épices","champignons", "oignons", })
 };
 
 foreach (var pizza in listePizzas) {
@@ -19,15 +19,20 @@ class Pizza
     string nom { get; set; }
     float prix { get; set; }
     bool vegetarienne {  get; set; }
-    public Pizza(string nom, float prix, bool vegetarienne) 
+
+    List<string> ingredients { get; set; }
+    public Pizza(string nom, float prix, bool vegetarienne, List<string> ingredients)
     {
         this.nom = nom;
         this.prix = prix;
         this.vegetarienne = vegetarienne;
+        this.ingredients = ingredients;
     }
 
-    private static string FormaterNomPizza(string s)
+    private static string FormaterEcriture(string s)
     {
+        if (string.IsNullOrEmpty(s))
+            return s;
         string minuscules = s.ToLower();
         string majuscules = s.ToUpper();
         string resultat = majuscules[0] + minuscules[1..];
@@ -35,9 +40,16 @@ class Pizza
     }
     public void Afficher()
     {
-        string IndicationVege = vegetarienne ? " (V) " : ";";
-        string nomAffiche = FormaterNomPizza(nom);
+        string IndicationVege = vegetarienne ? " (V) " : "";
+        string nomAffiche = FormaterEcriture(nom);
+        List<string> ingredientsAffiches = new List<string>();
+        foreach (var ingredient in ingredients)
+        {
+            ingredientsAffiches.Add(FormaterEcriture(ingredient));
+        }
         Console.WriteLine(nomAffiche + IndicationVege + " - " + prix + "€");
+        Console.WriteLine(string.Join(", ", ingredientsAffiches));
+        Console.WriteLine();
     }
 }
 
