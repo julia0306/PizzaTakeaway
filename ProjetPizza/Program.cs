a5f68b2f0f6f292f2c1296fa5950720199530c1e
julia0306 <123451628+julia0306@users.noreply.github.com>
2024-02-24 15:48:06 +0100

    Ajoutez des fichiers projet.

---
+++ ProjetPizza/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations.Schema;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Hello, World!");
+var pizza1 = new Pizza("4 fromages", 11.5f, true);
+pizza1.Afficher();
+
+
+class Pizza
+{
+    string nom { get; set; }
+    float prix { get; set; }
+    bool vegetarienne {  get; set; }
+    public Pizza(string nom, float prix, bool vegetarienne) 
+    {
+        this.nom = nom;
+        this.prix = prix;
+        this.vegetarienne = vegetarienne;
+    }
+
+    public void Afficher()
+    {
+        Console.WriteLine(nom + "-" + prix + "€");
+    }
+}
+
+
